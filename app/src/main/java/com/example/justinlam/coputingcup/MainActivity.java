711715067b21685e67b531999f30f1914448c1ac
PeaceEmbankment <peaceembankment@gmail.com>
2017-03-18 21:09:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/justinlam/coputingcup/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.justinlam.coputingcup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void newthing(View view){
+
+    }
+    public void importtxt(View view){
+
+    }
+    public void exporttxt(View view){
+
+    }
+    public void viewcalendar(View view){
+
+    }
+}
