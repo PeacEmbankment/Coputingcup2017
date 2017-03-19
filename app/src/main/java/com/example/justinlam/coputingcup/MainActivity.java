405f060544b7b7b40025dfeabd17472ca9bf7e28
PeaceEmbankment <peaceembankment@gmail.com>
2017-03-19 23:39:38 +0800

    change on 15:12 19/3/2017

--- app/src/main/java/com/example/justinlam/coputingcup/MainActivity.java
+++ app/src/main/java/com/example/justinlam/coputingcup/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void newthing(View view){
+        Log.d("new thing void","");
         Intent open_game1 = new Intent(context, addthingincalendar.class);
+        Log.d("created intent","");
         context.startActivity(open_game1);
 
     }
--- app/src/main/java/com/example/justinlam/coputingcup/addthingincalendar.java
+++ app/src/main/java/com/example/justinlam/coputingcup/addthingincalendar.java
@@ -2,17 +2,16 @@ package com.example.justinlam.coputingcup;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Point;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class addthingincalendar {
+public class addthingincalendar extends AppCompatActivity{
 
-    public class Gotogame extends Activity {
         Context context;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.addthingincalendarxml);
         }
-    }
+
 }
