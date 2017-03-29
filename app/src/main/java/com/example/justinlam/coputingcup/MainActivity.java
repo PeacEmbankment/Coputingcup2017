d90a04ace717cbc8bede91134fe2524015691a45
PeaceEmbankment <peaceembankment@gmail.com>
2017-03-29 22:01:28 +0800

    commit to github 29/3/2017

--- app/src/main/java/com/example/justinlam/coputingcup/MainActivity.java
+++ app/src/main/java/com/example/justinlam/coputingcup/MainActivity.java
@@ -2,10 +2,22 @@ package com.example.justinlam.coputingcup;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
@@ -28,10 +40,50 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void importtxt(View view){
+        String aDataRow = "";
+        try {
+            File myFile = new File("/sdcard/testimport.txt");
+            FileInputStream fIn = new FileInputStream(myFile);
+            BufferedReader myReader = new BufferedReader(
+                    new InputStreamReader(fIn));
 
+            while ((aDataRow = myReader.readLine()) != null) {
+                Log.d("MainActivity","LogaDataRow: " + aDataRow);
+            }
+            myReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public void exporttxt(View view){
+        Log.d("entered","exporttxt");
+        File patternDirectory = new File(Environment.getExternalStorageDirectory().getAbsolutePath().toString()+"/sdcard/exporttest.txt");
+        patternDirectory.mkdirs();
+        String filename = "/sdcard/testexport.txt";
+        String string = "Hello world!";
+        FileOutputStream outputStream;
 
+        try {
+            Log.d("entered","exporttxt try catch");
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream = new FileOutputStream (new File(patternDirectory.getAbsolutePath().toString()), true);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            Log.d("MainActivity","e:"+e);
+        }
+    }
+    public File getTempFile(Context context, String url) {
+        File file;
+        file = new File("hihihi");
+        try {
+            String fileName = Uri.parse(url).getLastPathSegment();
+            file = File.createTempFile(fileName, null, context.getCacheDir());
+        } catch (IOException e) {
+            // Error while creating file
+        }
+        return file;
     }
     public void viewcalendar(View view){
         Intent open_game1 = new Intent(getBaseContext(), calendar.class);
